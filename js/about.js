7d33eef73216390abed11d231ef773db297cb87c
Ivan Quon <ivanson2300@gmail.com>
2025-10-25 16:24:09 -0700

    progress

--- js/about.js
+++ js/about.js
@@ -1,20 +1,26 @@
 function parseTweets(runkeeper_tweets) {
-	//Do not proceed if no tweets loaded
-	if(runkeeper_tweets === undefined) {
-		window.alert('No tweets returned');
-		return;
-	}
+    //Do not proceed if no tweets loaded
+    if (runkeeper_tweets === undefined) {
+        window.alert("No tweets returned")
+        return
+    }
 
-	tweet_array = runkeeper_tweets.map(function(tweet) {
-		return new Tweet(tweet.text, tweet.created_at);
-	});
-	
-	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
-	//It works correctly, your task is to update the text of the other tags in the HTML file!
-	document.getElementById('numberTweets').innerText = tweet_array.length;	
+    tweet_array = runkeeper_tweets.map(function (tweet) {
+        return new Tweet(tweet.text, tweet.created_at)
+    })
+
+    //This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
+    //It works correctly, your task is to update the text of the other tags in the HTML file!
+    document.getElementById("numberTweets").innerText = tweet_array.length
+    document.getElementsByClassName("completedEvents").forEach((element) => {
+        element.innerText = tweet_array.filter((tweet) => tweet.source === "completed_event").length
+    })
+    document.getElementsByClassName("liveEvents").innerText = tweet_array.reduce()
+    document.getElementsByClassName("achievements").innerText = tweet_array.reduce()
+    document.getElementsByClassName("miscellaneous").innerText = tweet_array.reduce()
 }
 
 //Wait for the DOM to load
-document.addEventListener('DOMContentLoaded', function (event) {
-	loadSavedRunkeeperTweets().then(parseTweets);
-});
+document.addEventListener("DOMContentLoaded", function (event) {
+    loadSavedRunkeeperTweets().then(parseTweets)
+})
