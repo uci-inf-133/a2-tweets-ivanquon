159dd3d7f6744c359f28231bf30fef50247b5228
Ivan Quon <70609060+ivanquon@users.noreply.github.com>
2025-10-26 04:56:07 -0700

    Dates + Formatting

--- js/about.js
+++ js/about.js
@@ -11,6 +11,23 @@ function parseTweets(runkeeper_tweets) {
 
   //This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
   //It works correctly, your task is to update the text of the other tags in the HTML file!
+
+  //Dates
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  document.getElementById("firstDate").innerText = tweet_array
+    .sort((t1, t2) => t1.time - t2.time)
+    .at(0)
+    .time.toLocaleDateString("en-US", options);
+  document.getElementById("lastDate").innerText = tweet_array
+    .sort((t1, t2) => t1.time - t2.time)
+    .at(-1)
+    .time.toLocaleDateString("en-US", options);
+  //Counts
   document.getElementById("numberTweets").innerText = tweet_array.length;
   [...document.getElementsByClassName("completedEvents")].forEach((element) => {
     element.innerText = tweet_array.filter((tweet) => tweet.source === "completed_event").length;
@@ -28,29 +45,33 @@ function parseTweets(runkeeper_tweets) {
     element.innerText = tweet_array.filter((tweet) => tweet.written).length;
   });
 
+  //Percentages
   [...document.getElementsByClassName("completedEventsPct")].forEach((element) => {
     element.innerText =
       math.format(
         (tweet_array.filter((tweet) => tweet.source === "completed_event").length / tweet_array.length) * 100,
-        { precision: 2 }
+        { precision: 2, notation: "fixed" }
       ) + "%";
   });
   [...document.getElementsByClassName("liveEventsPct")].forEach((element) => {
     element.innerText =
       math.format((tweet_array.filter((tweet) => tweet.source === "live_event").length / tweet_array.length) * 100, {
         precision: 2,
+        notation: "fixed",
       }) + "%";
   });
   [...document.getElementsByClassName("achievementsPct")].forEach((element) => {
     element.innerText =
       math.format((tweet_array.filter((tweet) => tweet.source === "achievement").length / tweet_array.length) * 100, {
         precision: 2,
+        notation: "fixed",
       }) + "%";
   });
   [...document.getElementsByClassName("miscellaneousPct")].forEach((element) => {
     element.innerText =
       math.format((tweet_array.filter((tweet) => tweet.source === "miscellaneous").length / tweet_array.length) * 100, {
         precision: 2,
+        notation: "fixed",
       }) + "%";
   });
   [...document.getElementsByClassName("writtenPct")].forEach((element) => {
@@ -59,7 +80,7 @@ function parseTweets(runkeeper_tweets) {
         (tweet_array.filter((tweet) => tweet.written).length /
           tweet_array.filter((tweet) => tweet.source === "completed_event").length) *
           100,
-        { precision: 2 }
+        { precision: 2, notation: "fixed" }
       ) + "%";
   });
 }
