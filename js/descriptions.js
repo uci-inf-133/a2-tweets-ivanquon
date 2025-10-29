b45742e1efea1103a23f9fee1a50e6ae44b881bf
Ivan Quon <70609060+ivanquon@users.noreply.github.com>
2025-10-29 04:11:20 -0700

    Reload and initial load adjustments

--- js/descriptions.js
+++ js/descriptions.js
@@ -13,21 +13,27 @@ function parseTweets(runkeeper_tweets) {
 
   //Initial load or reloads
   document.getElementById("searchText").innerText = document.getElementById("textFilter").value.toLowerCase();
-  document.getElementById("searchCount").innerText = written_tweets.filter((tweet) =>
-    tweet.writtenText.toLowerCase().includes(document.getElementById("textFilter").value.toLowerCase())
-  ).length;
-  document.getElementById("tweetTable").innerHTML = written_tweets
-    .filter((tweet) =>
+  if (!document.getElementById("textFilter").value) {
+    document.getElementById("searchCount").innerText = 0;
+    document.getElementById("tweetTable").innerHTML = "";
+  } else {
+    document.getElementById("searchCount").innerText = written_tweets.filter((tweet) =>
       tweet.writtenText.toLowerCase().includes(document.getElementById("textFilter").value.toLowerCase())
-    )
-    .map((tweet, index) => tweet.getHTMLTableRow(index + 1))
-    .join("");
+    ).length;
+
+    document.getElementById("tweetTable").innerHTML = written_tweets
+      .filter((tweet) =>
+        tweet.writtenText.toLowerCase().includes(document.getElementById("textFilter").value.toLowerCase())
+      )
+      .map((tweet, index) => tweet.getHTMLTableRow(index + 1))
+      .join("");
+  }
 }
 
 function addEventHandlerForSearch() {
   document.getElementById("textFilter").addEventListener("input", () => {
     document.getElementById("searchText").innerText = document.getElementById("textFilter").value.toLowerCase();
-    if (!document.getElementById("textFilter").value.toLowerCase()) {
+    if (!document.getElementById("textFilter").value) {
       document.getElementById("searchCount").innerText = 0;
       document.getElementById("tweetTable").innerHTML = "";
     } else {
